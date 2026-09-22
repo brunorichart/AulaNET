40013f8f5a54c2eff7a3e925739f0d1dd1ed767d
Bruno <brunorichart19@gmail.com>
2026-09-22 18:15:56 -0300

    Globalizando a lógica de acesso ao banco de dados

---
+++ AulaNET/Database/Database.vb
@@ -0,0 +1,15 @@
+﻿Imports Microsoft.Data.SqlClient
+Imports System.Configuration
+
+Public Class Database
+
+    Private Shared ReadOnly connectionString As String =
+        ConfigurationManager.ConnectionStrings("MeuBanco").ConnectionString
+
+    Public Shared Function GetConnection() As SqlConnection
+
+        Return New SqlConnection(connectionString)
+
+    End Function
+
+End Class
--- AulaNET/frmForm.vb
+++ AulaNET/frmForm.vb
@@ -1,4 +1,6 @@
-﻿Public Class frmForm
+﻿Imports Microsoft.Data.SqlClient
+
+Public Class frmForm
     Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
         Dim StrName As String = txtName.Text
         Dim StrEmail As String = txtEmail.Text
@@ -8,5 +10,22 @@
         MessageBox.Show("Nome: " & StrName & vbCrLf &
                         "Email: " & StrEmail & vbCrLf &
                         "Telefone: " & StrPhone, "Informações do Usuário", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+
+        Using conexao As SqlConnection = Database.GetConnection()
+
+            Try
+
+                conexao.Open()
+
+                MessageBox.Show("Conectado com sucesso!")
+
+            Catch ex As Exception
+
+                MessageBox.Show("Erro: " & ex.Message)
+
+            End Try
+
+        End Using
     End Sub
 End Class
