62f13a1cffa49665df901d6e109488224ceb7262
Bruno <brunorichart19@gmail.com>
2026-09-23 13:40:30 -0300

    Criação do designer da calculadora

--- AulaNET/frmCalc.Designer.vb
+++ AulaNET/frmCalc.Designer.vb
@@ -22,16 +22,251 @@ Partial Class frmCalc
     'Não o modifique usando o editor de códigos.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        Button12 = New Button()
+        Button13 = New Button()
+        Button14 = New Button()
+        Button15 = New Button()
+        Button16 = New Button()
+        Button17 = New Button()
+        TextBox1 = New TextBox()
         SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(18, 280)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(60, 57)
+        Button1.TabIndex = 0
+        Button1.Text = "0"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Font = New Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button2.Location = New Point(84, 280)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(60, 57)
+        Button2.TabIndex = 0
+        Button2.Text = "."
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Font = New Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button3.Location = New Point(150, 280)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(60, 57)
+        Button3.TabIndex = 0
+        Button3.Text = "C"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Font = New Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button4.Location = New Point(216, 280)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(60, 57)
+        Button4.TabIndex = 0
+        Button4.Text = "+"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Font = New Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button5.Location = New Point(18, 217)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(60, 57)
+        Button5.TabIndex = 0
+        Button5.Text = "1"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Font = New Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button6.Location = New Point(84, 217)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(60, 57)
+        Button6.TabIndex = 0
+        Button6.Text = "2"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Font = New Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button7.Location = New Point(150, 217)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(60, 57)
+        Button7.TabIndex = 0
+        Button7.Text = "3"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.Font = New Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button8.Location = New Point(216, 217)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(60, 57)
+        Button8.TabIndex = 0
+        Button8.Text = "-"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' Button9
+        ' 
+        Button9.Font = New Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button9.Location = New Point(18, 154)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(60, 57)
+        Button9.TabIndex = 0
+        Button9.Text = "4"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' Button10
+        ' 
+        Button10.Font = New Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button10.Location = New Point(84, 154)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(60, 57)
+        Button10.TabIndex = 0
+        Button10.Text = "5"
+        Button10.UseVisualStyleBackColor = True
+        ' 
+        ' Button11
+        ' 
+        Button11.Font = New Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button11.Location = New Point(150, 154)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(60, 57)
+        Button11.TabIndex = 0
+        Button11.Text = "6"
+        Button11.UseVisualStyleBackColor = True
+        ' 
+        ' Button12
+        ' 
+        Button12.Font = New Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button12.Location = New Point(216, 154)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(60, 57)
+        Button12.TabIndex = 0
+        Button12.Text = "*"
+        Button12.UseVisualStyleBackColor = True
+        ' 
+        ' Button13
+        ' 
+        Button13.Font = New Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button13.Location = New Point(18, 91)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(60, 57)
+        Button13.TabIndex = 0
+        Button13.Text = "7"
+        Button13.UseVisualStyleBackColor = True
+        ' 
+        ' Button14
+        ' 
+        Button14.Font = New Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button14.Location = New Point(84, 91)
+        Button14.Name = "Button14"
+        Button14.Size = New Size(60, 57)
+        Button14.TabIndex = 0
+        Button14.Text = "8"
+        Button14.UseVisualStyleBackColor = True
+        ' 
+        ' Button15
+        ' 
+        Button15.Font = New Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button15.Location = New Point(150, 91)
+        Button15.Name = "Button15"
+        Button15.Size = New Size(60, 57)
+        Button15.TabIndex = 0
+        Button15.Text = "9"
+        Button15.UseVisualStyleBackColor = True
+        ' 
+        ' Button16
+        ' 
+        Button16.Font = New Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button16.Location = New Point(216, 91)
+        Button16.Name = "Button16"
+        Button16.Size = New Size(60, 57)
+        Button16.TabIndex = 0
+        Button16.Text = "/"
+        Button16.UseVisualStyleBackColor = True
+        ' 
+        ' Button17
+        ' 
+        Button17.Font = New Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button17.Location = New Point(18, 343)
+        Button17.Name = "Button17"
+        Button17.Size = New Size(258, 57)
+        Button17.TabIndex = 1
+        Button17.Text = "="
+        Button17.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(18, 13)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(258, 72)
+        TextBox1.TabIndex = 2
         ' 
         ' frmCalc
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(353, 450)
+        ClientSize = New Size(294, 413)
+        Controls.Add(TextBox1)
+        Controls.Add(Button17)
+        Controls.Add(Button16)
+        Controls.Add(Button12)
+        Controls.Add(Button8)
+        Controls.Add(Button4)
+        Controls.Add(Button15)
+        Controls.Add(Button11)
+        Controls.Add(Button7)
+        Controls.Add(Button3)
+        Controls.Add(Button14)
+        Controls.Add(Button10)
+        Controls.Add(Button6)
+        Controls.Add(Button2)
+        Controls.Add(Button13)
+        Controls.Add(Button9)
+        Controls.Add(Button5)
+        Controls.Add(Button1)
         MaximizeBox = False
         Name = "frmCalc"
+        StartPosition = FormStartPosition.CenterScreen
         Text = "Calculadora"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents TextBox1 As TextBox
 End Class
