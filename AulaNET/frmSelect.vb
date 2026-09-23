4d4aad2c14ad4c7db2bfd28d4d53fb1edf67f8c6
Bruno <brunorichart19@gmail.com>
2026-09-23 11:48:57 -0300

    Implementação do select

--- AulaNET/frmSelect.vb
+++ AulaNET/frmSelect.vb
@@ -1,3 +1,11 @@
 ﻿Public Class frmSelect
+    Private Sub btnSelect_Click(sender As Object, e As EventArgs) Handles btnSelect.Click
+        lbSelected.Items.Add(lbSelect.SelectedItem)
+    End Sub
 
+    Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
+        If lbSelected.SelectedItem IsNot Nothing Then
+            lbSelected.Items.Remove(lbSelected.SelectedItem)
+        End If
+    End Sub
 End Class
