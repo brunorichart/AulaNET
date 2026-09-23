bfe0e0237156a6e3b3295e5f9f2d6a4b0d052ff1
Bruno <brunorichart19@gmail.com>
2026-09-23 17:21:07 -0300

    Implementando o igual

--- AulaNET/frmCalc.vb
+++ AulaNET/frmCalc.vb
@@ -1,6 +1,6 @@
 ﻿Public Class frmCalc
     Private Sub Number_Click(sender As Object, e As EventArgs) Handles Button5.Click, Button6.Click,
-        Button7.Click, Button9.Click, Button10.Click, Button11.Click, Button12.Click, Button13.Click,
+        Button7.Click, Button9.Click, Button10.Click, Button11.Click, Button13.Click,
         Button14.Click, Button15.Click
 
         Dim button As Button = CType(sender, Button)
@@ -28,5 +28,18 @@
 
     Private Sub Button3_Clear(sender As Object, e As EventArgs) Handles Button3.Click
         TextBox1.Text = String.Empty
+    End Sub
+
+    Private Sub Operators(sender As Object, e As EventArgs) Handles Button4.Click, Button8.Click, Button12.Click, Button16.Click
+
+        TextBox1.Text &= " " & CType(sender, Button).Text
+    End Sub
+
+    Private Sub EqualsClick(sender As Object, e As EventArgs) Handles Button17.Click
+        Dim expression As String = TextBox1.Text
+        Dim result As Object
+        Dim table As New DataTable
+        result = table.Compute(expression, String.Empty)
+        TextBox1.Text = result.ToString
     End Sub
 End Class
